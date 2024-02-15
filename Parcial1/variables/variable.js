29ae9c24ae219f2fbe8952dfff0a87bf262468fc
AlexanderCastilloMurga <alex.strange.1920@gmail.com>
2024-02-14 22:22:58 -0600

    first commit

---
+++ Parcial1/variables/variable.js
@@ -0,0 +1,20 @@
+var nombre="adsoft"
+var email="adsoft@live.com.mx"
+var edad = 28
+
+function getNombre() {
+	console.log(nombre;
+		var myNameElement = document.getElementById("nombre");
+		myNameElement.innerHTML = nombre;
+}
+function getEmail(){
+	console.log(email);
+	var myEmailElement = document.getElementById("email");
+	myEmailElement.innerHTML = '<b>' + email + '</b>';
+}
+function getEdad(){
+	console.log(edad);
+	var myEdadElement = document.getElemtById("edad");
+	myEdadElement.innerHTML = <'h1'> + edad '</h1>';
+	myEdadElement.style.color = "#0000FF";
+}
